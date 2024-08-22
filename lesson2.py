f8e76bce1eb311f2d47193d0758ef0a03e0105f8
Yuliya-str <gliceriya@yandex.ru>
2024-08-22 23:50:44 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+completed_tasks=12
+hours_spent=1.5
+course_title='Python'
+time_per_job=0.125
+print(course_title,completed_tasks,hours_spent,time_per_job )
+print('Курс:',course_title ,',всего задач:',completed_tasks,','
+'количество затраченных часов:',hours_spent,',время на одно задание:',time_per_job,'.')

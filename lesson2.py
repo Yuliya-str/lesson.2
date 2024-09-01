2bf73bf5c07a825c7850a1ea6545cebd548854cb
Yuliya-str <gliceriya@yandex.ru>
2024-09-01 16:34:09 +0300

    Add files via upload

--- lesson2.py
+++ lesson2.py
@@ -2,5 +2,4 @@ completed_tasks=12
 hours_spent=1.5
 course_title='Python'
 time_per_job=hours_spent / completed_tasks
-print(course_title,completed_tasks,hours_spent,time_per_job )
 print(course_title , time_per_job)
